6ff2ef0da010a067aa6d0441bd6adea02495c1e6
Amy Silva <amy_silva@me.com>
2018-08-08 08:49:57 -0700

    added move count on image click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+let clicks = 0;
+
+$('.catImg').click(clickedCat);
 $('.aboutHeader').click(toggleAbout);
+
+function clickedCat () {
+  clicks++;
+  console.log('cat clicked!');
+  console.log(`moves = ${clicks}`);
+}
 
 function toggleAbout () {
   console.log('about clicked!');
